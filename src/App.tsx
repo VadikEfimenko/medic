d7f92be7084dc4541a3b6dc571021c383c732cf0
“Vadim <vadik.efimencko@gmail.com>
2022-05-29 12:51:15 +0300

    Draft version

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png';
+import telegram from './images/icons/telegram.svg';
+import zen from './images/icons/zen.svg';
+import arrow from './images/icons/arrow.svg';
+import me from './images/me.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://vk.com/penkinavl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Привет, медик!
-        </a>
+        <div className="headerBackground">
+            <img src={logo} className="logo" alt="logo" />
+            <button
+                onClick={() => {
+                    window.location.assign('https://disk.yandex.ru/d/t3otFOcURqyvYg');
+                }}
+                className="mainLink"
+            >
+                Материалы
+
+                <img src={arrow} className="arrowIcon" alt="logo" />
+            </button>
+        </div>
       </header>
+      <div className="social">
+          <div className="controls">
+              <div
+                  className="telegram socialItem"
+                  onClick={() => window.location.assign('https://t.me/valeria_medic')}
+              >
+                  <img src={telegram} alt="logo" />
+              </div>
+
+              <div
+                  className="zen socialItem"
+                  onClick={() => window.location.assign('https://zen.yandex.ru/valeria_medic')}
+              >
+                  <img src={zen} alt="logo" />
+              </div>
+          </div>
+      </div>
+      <div className="aboutMe">
+          <h1>Обо мне</h1>
+          <div className="aboutSection">
+              <div className="aboutImage">
+                  <img src={me} alt="logo" />
+              </div>
+
+              <div className="aboutText">
+                  Всем привет, меня зовут Валерия🤍 Я студентка 4 курса ПМГМУ им. И.М. Сеченова.
+                  И если честно я очень люблю медицину. Несмотря на то, что существует стереотип,
+                  что в медицинском нет личной жизни, да и вообще ничего нет, кроме учёбы - это далеко не так.
+                  В своём блоге я делюсь моментами из жизни, путешествиями, вдохновением и мотивацией, не обходится
+                  дело и без конспектов и полезных материалов. Искренне считаю, что учиться вместе, поддерживать друг
+                  друга и делиться полученной информацией - это бесценно. Поэтому Welcome in medicine world 🌎
+              </div>
+          </div>
+      </div>
+      <footer>
+          <div
+              onClick={() => window.location.assign('https://t.me/vefimencko')}
+          >
+              Design by @vefimenko
+          </div>
+      </footer>
     </div>
   );
 }
